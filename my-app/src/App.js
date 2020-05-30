315e9aea67f2f7c54a374dce13734b8f690ed3db
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 16:57:38 +0200

    Removed stuff which is in return()
    Because we create something new

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,13 +11,7 @@ const App = () => {
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
   return (
-    <div>
-       <h1>Hello React World</h1>
-       {/* onClick is compiled as JSX and because of the {} after onClick-attribute the code inside {} gets compiled as Javascript */}
-       <h2 onClick={handleClick}> {/* The curly braces tells the compiler that it should execute the code as JavaScript */}
-             {message}
-       </h2>
-    </div>
+   
   );
 }
 //We're also going to export this function so we can use it elsewhere
