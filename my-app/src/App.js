91b104fdc366982d4b0e35061e9368aaf73fb567
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 16:23:41 +0200

    The first component
    No commands used
    Code is described in code

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+//This brings the React library into scope and gives us access to all the cool features
+import React from "react";
+//That's more or less what a component is
+const App = () => {
+  //This is what get’s returned from this component
+  //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
+  return (
+    <div>
+       <h1>Hello React World</h1>
+       <h2>
+             This is our first React App - isn't it marvellous?!
+       </h2>
+    </div>
+  );
+}
+//We're also going to export this function so we can use it elsewhere
+export default App;
