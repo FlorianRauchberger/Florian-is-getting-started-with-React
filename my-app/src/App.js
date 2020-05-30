90cc18639bdf7d9a7d4e6c86e6ed65a53eb950a3
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 16:41:46 +0200

    Make things dynamic
    code is explained in code
    Info: create comments in JSX with {/* */}

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@
 import React from "react";
 //That's more or less what a component is
 const App = () => {
+  //message holds our message and in return() the {message} shows our message
+  const message = "This is my first variable rendered in JSX";
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
   return (
     <div>
        <h1>Hello React World</h1>
-       <h2>
-             This is our first React App - isn't it marvellous?!
+       <h2> {/* The curly braces tells the compiler that it should execute the code as JavaScript */}
+             {message}
        </h2>
     </div>
   );
