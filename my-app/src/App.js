a68c3d8b46087aca91635f9b90ba7d9d84f227d1
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 17:06:14 +0200

    Creating Contact Card
    code is explained in code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,15 @@ const App = () => {
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
   return (
-   
+    
+    <div className="contact-card"> {/*To acces CSS classes/id's we need to use className because class is reserved for JS*/}
+    <img src="https://via.placeholder.com/150" alt="profile" />
+    <div className="user-details">
+            <p>Name: Florian Nairolf</p>
+            <p>Email: Florian@reactjs.org</p>
+            <p>Age: 16</p>
+    </div>
+</div>
   );
 }
 //We're also going to export this function so we can use it elsewhere
