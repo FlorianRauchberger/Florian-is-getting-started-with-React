3a24d4780a1a8a8591030d3ccb188bf1b009aad8
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 16:59:24 +0200

    also removed const's
    because we don't need it anymore

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,6 @@
 import React from "react";
 //That's more or less what a component is
 const App = () => {
-  //message holds our message and in return() the {message} shows our message
-  const message = "This is my first variable rendered in JSX";
-  //We created a function which get's called when someone clicks our h2
-  const handleClick = () =>{
-    alert("you clicked the message!");
-}
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
   return (
