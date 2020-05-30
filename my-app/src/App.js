4c10ef235360f210a8723e38e3ab2f694e94c066
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 16:50:36 +0200

    Calling functions
    code is described in code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,17 @@ import React from "react";
 const App = () => {
   //message holds our message and in return() the {message} shows our message
   const message = "This is my first variable rendered in JSX";
+  //We created a function which get's called when someone clicks our h2
+  const handleClick = () =>{
+    alert("you clicked the message!");
+}
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
   return (
     <div>
        <h1>Hello React World</h1>
        {/* onClick is compiled as JSX and because of the {} after onClick-attribute the code inside {} gets compiled as Javascript */}
-       <h2 onClick={()=> alert("you clicked the message!")}> {/* The curly braces tells the compiler that it should execute the code as JavaScript */}
+       <h2 onClick={handleClick}> {/* The curly braces tells the compiler that it should execute the code as JavaScript */}
              {message}
        </h2>
     </div>
