7b7cf813f38480bdb2a227f0d4dc02a4631bae73
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2020-05-30 17:15:12 +0200

    Making Code Reusable/multiple time renderable
    code is described in code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,17 +5,27 @@ import './style.css'
 const App = () => {
   //This is what get’s returned from this component
   //Info: <div> <h1> etc. are NOT HTML tags. They are called JSX (JavaScript XML). This basically allows us to mix JavaScript and HTML together ^^
+  //This renders 3 times ContactCard. With exporting/importing you can render things from file 1 in file 2. Very useful when you have a Website with i.e a Header. You change the header one time in a file and it applies to all sites i.e Contact/Blog/Home.
   return (
-    
-    <div className="contact-card"> {/*To acces CSS classes/id's we need to use className because class is reserved for JS*/}
-    <img src="https://via.placeholder.com/150" alt="profile" />
-    <div className="user-details">
-            <p>Name: Florian Nairolf</p>
-            <p>Email: Florian@reactjs.org</p>
-            <p>Age: 16</p>
-    </div>
-</div>
+    <>
+      <ContactCard />
+      <ContactCard />
+      <ContactCard />
+    </>
   );
 }
+
+const ContactCard = () => {
+  return (
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Florian Niarolf</p>
+        <p>Email: Florian@reactjs.org</p>
+        <p>Age: 16</p>
+      </div>
+    </div>
+  );
+};
 //We're also going to export this function so we can use it elsewhere
 export default App;
